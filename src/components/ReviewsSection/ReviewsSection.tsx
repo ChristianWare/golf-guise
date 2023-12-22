8031d4ced96a7aa734360574f3d09cf6dfeacc4a
Chris Ware <c.p.ware@hotmail.com>
2023-12-22 14:45:39 -0700

    Review Section update

--- src/components/ReviewsSection/ReviewsSection.tsx
+++ src/components/ReviewsSection/ReviewsSection.tsx
@@ -5,6 +5,7 @@ import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./ReviewsSection.module.css";
 import BlogPreviewLarge from "../BlogPreviewLarge/BlogPreviewLarge";
 import BlogPreviewMedium from "../BlogPreviewMedium/BlogPreviewMedium";
+import BlogPreviewSmall from "../BlogPreviewSmall/BlogPreviewSmall";
 
 const Reviews: FC<BlogSectionProps> = ({ blogData }) => {
   return (
@@ -16,20 +17,40 @@ const Reviews: FC<BlogSectionProps> = ({ blogData }) => {
           </div>
           <div className={styles.bottom}>
             <div className={styles.left}>
-              {blogData.slice(0, 1).map((x: BlogData, index: number) => (
-                <BlogPreviewLarge
-                  key={index}
-                  mapData={x}
-                  column='column'
-                  imgCol='imgCol'
-                  colPadding='colPadding'
-                />
-              ))}
+              <div className={styles.leftTop}>
+                {blogData.slice(0, 1).map((x: BlogData, index: number) => (
+                  <BlogPreviewLarge
+                    key={index}
+                    mapData={x}
+                    column='column'
+                    imgCol='imgCol'
+                    colPadding='colPadding'
+                  />
+                ))}
+              </div>
+              <div className={styles.leftBottom}>
+                {blogData.slice(0, 2).map((x: BlogData, index: number) => (
+                  <BlogPreviewSmall
+                    key={index}
+                    mapData={x}
+                    column='column'
+                    imgCol='imgCol'
+                    colPadding='colPadding'
+                  />
+                ))}
+              </div>
             </div>
             <div className={styles.right}>
-              {blogData.slice(0, 3).map((x: BlogData, index: number) => (
-                <BlogPreviewMedium key={index} mapData={x} />
-              ))}
+              <div className={styles.rightTop}>
+                {blogData.slice(0, 2).map((x: BlogData, index: number) => (
+                  <BlogPreviewMedium key={index} mapData={x} />
+                ))}
+              </div>
+              <div className={styles.rightBottom}>
+                <h3 className={styles.heading2}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, voluptate! Inventore ut labore voluptatem ab.
+                </h3>
+              </div>
             </div>
           </div>
         </ContentPadding>
