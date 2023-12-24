223371d6c84163d66f39c02443273353c4cce4d5
Chris Ware <c.p.ware@hotmail.com>
2023-12-24 16:24:18 -0700

    Insta Done

--- src/components/InstaFeed/InstaFeed.tsx
+++ src/components/InstaFeed/InstaFeed.tsx
@@ -1,6 +1,8 @@
 import ContentPadding from "../ContentPadding/ContentPadding";
 import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./InstaFeed.module.css";
+import Image from "next/image";
+import Img from "../../../public/images/img1.png";
 
 const InstaFeed = () => {
   return (
@@ -10,7 +12,20 @@ const InstaFeed = () => {
           <div className={styles.top}>
             <h2 className={styles.heading}>Lates from Instagram</h2>
           </div>
-          <div className={styles.bottom}>images here</div>
+          <div className={styles.bottom}>
+            <div className={styles.imgContainer}>
+              <Image src={Img} alt='image' fill className={styles.img} />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image src={Img} alt='image' fill className={styles.img} />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image src={Img} alt='image' fill className={styles.img} />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image src={Img} alt='image' fill className={styles.img} />
+            </div>
+          </div>
         </ContentPadding>
       </LayoutWrapper>
     </section>
