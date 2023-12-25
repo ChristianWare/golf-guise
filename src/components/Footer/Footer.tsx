297061dd583e5af75ca27c4fd976cdbf5fd3a697
Chris Ware <c.p.ware@hotmail.com>
2023-12-24 17:27:18 -0700

    Footer Start

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,25 @@
 import ContentPadding from "../ContentPadding/ContentPadding";
+import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.container}>
-      <ContentPadding>
-        <h2>Footer</h2>
-      </ContentPadding>
-    </footer>
+    <>
+      <LayoutWrapper>
+        <ContentPadding>
+          <footer className={styles.container}>
+            <h2>Footer</h2>
+          </footer>
+        </ContentPadding>
+      </LayoutWrapper>
+      <LayoutWrapper>
+        <ContentPadding>
+          <div className={styles.container}>
+            <p>Logo and disclosures here</p>
+          </div>
+        </ContentPadding>
+      </LayoutWrapper>
+    </>
   );
 };
 export default Footer;
