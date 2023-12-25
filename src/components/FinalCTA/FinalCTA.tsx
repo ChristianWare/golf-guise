00e811dcff0faea06ffb60d541388e6ded255a27
Chris Ware <c.p.ware@hotmail.com>
2023-12-24 17:17:28 -0700

    Final CTA Done

--- src/components/FinalCTA/FinalCTA.tsx
+++ src/components/FinalCTA/FinalCTA.tsx
@@ -1,16 +1,25 @@
+import Button from "../Button/Button";
 import ContentPadding from "../ContentPadding/ContentPadding";
 import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./FinalCTA.module.css";
 
 const FinalCTA = () => {
   return (
-    <section className={styles.container}>
-      <LayoutWrapper>
-        <ContentPadding>
-          <h2 className={styles.heading}>Final CTA Here</h2>
-        </ContentPadding>
-      </LayoutWrapper>
-    </section>
+    <LayoutWrapper>
+      <ContentPadding>
+        <section className={styles.container}>
+          <h2 className={styles.heading}>Treat your travel to cheap flights</h2>
+          <p className={styles.copy}>
+            Sign up for free to start getting flight alerts. Or upgrade to get
+            4x the deals, rare destinations, peak season tickets, and Mistake
+            Fares. Either way, you’ll never overpay on flights again.
+          </p>
+          <div className={styles.btnContainer}>
+            <Button btnType='primary' text='Get Going' href='/' />
+          </div>
+        </section>
+      </ContentPadding>
+    </LayoutWrapper>
   );
 };
 export default FinalCTA;
